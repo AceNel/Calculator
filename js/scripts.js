9d3a0c43735e7939b8ebb17c629455503120406e
Nelson Kimani <nelsonkimani96@gmail.com>
2019-08-14 07:29:49 +0300

    Add script.js file

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,5 @@ var add(number1, number2) {
 }
 var number1 = parseInt(prompt("Enter Number"));
 var number2 = parseInt(prompt("Enter Another Number"));
-alert(add(number1,number2));
+var results = add (number1, number2);
+alert(results);
