c360d7b2999d1f0d1ba84ee8c03f5525457f3930
Nelson Kimani <nelsonkimani96@gmail.com>
2019-08-14 07:26:26 +0300

    Add index.html file

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var add(number1, number2) {
+  return number1, number2;
+}
+var number1 = parseInt(prompt("Enter Number"));
+var number2 = parseInt(prompt("Enter Another Number"));
+alert(add(number1,number2));
